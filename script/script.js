2f4d87cbc95ad9ad7d7a80b70a480eee8b215bf6
Arjosh <ronaldjosiah.cruz@gmail.com>
2026-05-16 01:37:42 +0800

    Typing Effect

--- script/script.js
+++ script/script.js
@@ -99,3 +99,46 @@ backToTopBtn.addEventListener("click", () => {
         behavior: "smooth"
     });
 });
+
+const roles = [
+    "Web Developer",
+    "UI Designer",
+    "Full Stack Learner"
+];
+
+const typing = document.getElementById("typing");
+
+let roleIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+
+    const currentRole = roles[roleIndex];
+
+    if (!isDeleting) {
+        typing.textContent = currentRole.substring(0, charIndex + 1);
+        charIndex++;
+
+        if (charIndex === currentRole.length) {
+            isDeleting = true;
+
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+
+    } else {
+
+        typing.textContent = currentRole.substring(0, charIndex - 1);
+        charIndex--;
+
+        if (charIndex === 0) {
+            isDeleting = false;
+            roleIndex = (roleIndex + 1) % roles.length;
+        }
+    }
+
+    setTimeout(typeEffect, isDeleting ? 50 : 100);
+}
+
+typeEffect();
